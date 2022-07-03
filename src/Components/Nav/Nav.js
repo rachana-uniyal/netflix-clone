825e835ed525bee130425f22f218323df1832f5d
rachana-uniyal <rachanauniyal0417@gmail.com>
2022-07-03 19:12:17 +0530

    netflix-clone with firebase

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,36 @@
+import './Nav.css'
+import React, { useEffect, useState } from 'react'
+
+function Nav() {
+
+  const [show, handleShow] = useState(false)
+  
+  useEffect(()=>{
+    window.addEventListener("scroll",()=>{
+        if(window.scrollY > 100){
+            handleShow(true)
+        }else{
+            handleShow(false)
+        }
+    })
+    return ()=>{
+        window.removeEventListener("scroll",window)
+    }
+  },[])
+
+  return (
+    <div className={`nav ${show && "nav-black"}`}>
+        <img 
+        className='nav-logo'
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png?20190206123158"
+        alt="Netflix logo"
+        />
+        <img
+        className='nav-avatar'
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117"
+        alt="Netflix avatar" />
+    </div>
+  )
+}
+
+export default Nav
